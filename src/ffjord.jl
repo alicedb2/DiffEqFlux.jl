401c3a45ef81c48395e061c8c305cb840eba4aaa
alicedb2 <alice.doucet.beaupre@gmail.com>
2024-12-11 21:10:12 -0500

    ffjord refactor

--- src/ffjord.jl
+++ src/ffjord.jl
@@ -70,13 +70,14 @@ function FFJORD(
 end
 
 @inline function __trace_batched(x::AbstractArray{T, 3}) where {T}
-    return mapreduce(tr, vcat, eachslice(x; dims = 3); init = similar(x, 0))
+    # return mapreduce(tr, vcat, eachslice(x; dims = 3); init = similar(x, 0))
+    return tr.(eachslice(x; dims = 3))
 end
 
 @inline __norm_batched(x) = sqrt.(sum(abs2, x; dims = 1:(ndims(x) - 1)))
 
 function __ffjord(model::StatefulLuxLayer, u::AbstractArray{T, N}, p, ad = nothing,
-        regularize::Bool = false, monte_carlo::Bool = true) where {T, N}
+        regularize::Bool = false, monte_carlo::Bool = false, rng::AbstractRNG=default_rng(), mcdist::Symbol=:normal) where {T, N}
     L = size(u, N - 1)
     z = selectdim(u, N - 1, 1:(L - ifelse(regularize, 3, 1)))
     @set! model.ps = p
@@ -84,7 +85,11 @@ function __ffjord(model::StatefulLuxLayer, u::AbstractArray{T, N}, p, ad = nothi
     @assert size(mz) == size(z)
     if monte_carlo
         ad = ad === nothing ? AutoZygote() : ad
-        e = CRC.@ignore_derivatives randn!(similar(mz))
+        if mcdist === :rademacher
+            e = CRC.@ignore_derivatives rand!(rng, similar(mz), [one(T), -one(T)])
+        elseif mcdist === :normal
+            e = CRC.@ignore_derivatives randn!(rng, similar(mz))
+        end
         if ad isa AutoForwardDiff
             @assert !regularize "If `regularize = true`, then use `AutoZygote` instead."
             Je = Lux.jacobian_vector_product(model, AutoForwardDiff(), z, e)
@@ -111,7 +116,13 @@ function __ffjord(model::StatefulLuxLayer, u::AbstractArray{T, N}, p, ad = nothi
         if ad isa AutoForwardDiff || ad isa AutoZygote
             J = Lux.batched_jacobian(model, ad, z)
             trace_jac = reshape(__trace_batched(J), ntuple(i -> 1, N - 1)..., :)
-            e = CRC.@ignore_derivatives randn!(similar(mz))
+            if mcdist === :rademacher
+                e = CRC.@ignore_derivatives rand!(rng, similar(mz), [one(T), -one(T)])
+            elseif mcdist === :normal
+                e = CRC.@ignore_derivatives randn!(rng, similar(mz))
+            else
+                error("`mcdist` must be `:rademacher` or `:normal`.")
+            end
             eJ = reshape(batched_matmul(reshape(e, 1, :, size(e, N)), J), size(z))
         else
             error("`ad` must be `nothing` or `AutoForwardDiff` or `AutoZygote`.")
@@ -125,16 +136,16 @@ function __ffjord(model::StatefulLuxLayer, u::AbstractArray{T, N}, p, ad = nothi
     end
 end
 
-(n::FFJORD)(x, ps, st) = __forward_ffjord(n, x, ps, st)
+(n::FFJORD)(x, ps, st, rng) = __forward_ffjord(n, x, ps, st, rng)
 
-function __forward_ffjord(n::FFJORD, x::AbstractArray{T, N}, ps, st) where {T, N}
+function __forward_ffjord(n::FFJORD, x::AbstractArray{T, N}, ps, st, rng::AbstractRNG) where {T, N}
     S = size(x)
     (; regularize, monte_carlo) = st
     sensealg = InterpolatingAdjoint(; autojacvec = ZygoteVJP())
 
     model = StatefulLuxLayer{fixed_state_type(n.model)}(n.model, nothing, st.model)
 
-    ffjord(u, p, t) = __ffjord(model, u, p, n.ad, regularize, monte_carlo)
+    ffjord(u, p, t) = __ffjord(model, u, p, n.ad, regularize, monte_carlo, rng)
 
     _z = ChainRulesCore.@ignore_derivatives fill!(
         similar(x, S[1:(N - 2)]..., ifelse(regularize, 3, 1), S[N]), zero(T))
@@ -155,14 +166,21 @@ function __forward_ffjord(n::FFJORD, x::AbstractArray{T, N}, ps, st) where {T, N
         λ₁ = λ₂ = delta_logp
     end
 
+    # if n.basedist === nothing
+    #     logpz = -sum(abs2, z; dims = 1:(N - 1)) / T(2) .-
+    #             T(prod(S[1:(N - 1)]) / 2 * log(2π))
+    # else
+    #     logpz = logpdf(n.basedist, z)
+    # end
+    # logpx = reshape(logpz, 1, S[N]) .- delta_logp
+    # return (logpx, λ₁, λ₂), (; model = model.st, regularize, monte_carlo)
     if n.basedist === nothing
-        logpz = -sum(abs2, z; dims = 1:(N - 1)) / T(2) .-
-                T(prod(S[1:(N - 1)]) / 2 * log(2π))
+        logpz = zeros(T, S[N])
     else
         logpz = logpdf(n.basedist, z)
     end
     logpx = reshape(logpz, 1, S[N]) .- delta_logp
-    return (logpx, λ₁, λ₂), (; model = model.st, regularize, monte_carlo)
+    return (; logpx, delta_logp, z, λ₁, λ₂), (; model = model.st, regularize, monte_carlo)
 end
 
 __get_pred(sol::ODESolution) = last(sol.u)
@@ -172,30 +190,56 @@ function __backward_ffjord(::Type{T1}, n::FFJORD, n_samples::Int, ps, st, rng) w
     px = n.basedist
 
     if px === nothing
-        x = rng === nothing ? randn(T1, (n.input_dims..., n_samples)) :
-            randn(rng, T1, (n.input_dims..., n_samples))
+        # x = rng === nothing ? randn(T1, (n.input_dims..., n_samples)) :
+        #     randn(rng, T1, (n.input_dims..., n_samples))
+        randn(rng, T1, (n.input_dims..., n_samples))
     else
-        x = rng === nothing ? rand(px, n_samples) : rand(rng, px, n_samples)
+        # x = rng === nothing ? rand(px, n_samples) : rand(rng, px, n_samples)
+        rand(rng, px, n_samples)
     end
 
+    return __backward_ffjord(n, x, ps, st, rng)
+
+    # N, S, T = ndims(x), size(x), eltype(x)
+    # (; regularize, monte_carlo) = st
+    # sensealg = InterpolatingAdjoint(; autojacvec = ZygoteVJP())
+
+    # model = StatefulLuxLayer{true}(n.model, nothing, st.model)
+
+    # ffjord(u, p, t) = __ffjord(model, u, p, n.ad, regularize, monte_carlo)
+
+    # _z = ChainRulesCore.@ignore_derivatives fill!(
+    #     similar(x, S[1:(N - 2)]..., ifelse(regularize, 3, 1), S[N]), zero(T))
+
+    # prob = ODEProblem{false}(ffjord, cat(x, _z; dims = Val(N - 1)), reverse(n.tspan), ps)
+    # sol = solve(prob, n.args...; sensealg, n.kwargs...,
+    #     save_everystep = false, save_start = false, save_end = true)
+    # pred = __get_pred(sol)
+    # L = size(pred, N - 1)
+
+    # return selectdim(pred, N - 1, 1:(L - ifelse(regularize, 3, 1)))
+end
+
+function __backward_ffjord(n::FFJORD, x, ps, st, rng)
     N, S, T = ndims(x), size(x), eltype(x)
     (; regularize, monte_carlo) = st
     sensealg = InterpolatingAdjoint(; autojacvec = ZygoteVJP())
 
     model = StatefulLuxLayer{true}(n.model, nothing, st.model)
 
-    ffjord(u, p, t) = __ffjord(model, u, p, n.ad, regularize, monte_carlo)
+    ffjord(u, p, t) = __ffjord(model, u, p, n.ad, regularize, monte_carlo, rng)
 
     _z = ChainRulesCore.@ignore_derivatives fill!(
         similar(x, S[1:(N - 2)]..., ifelse(regularize, 3, 1), S[N]), zero(T))
-
     prob = ODEProblem{false}(ffjord, cat(x, _z; dims = Val(N - 1)), reverse(n.tspan), ps)
     sol = solve(prob, n.args...; sensealg, n.kwargs...,
         save_everystep = false, save_start = false, save_end = true)
     pred = __get_pred(sol)
     L = size(pred, N - 1)
 
-    return selectdim(pred, N - 1, 1:(L - ifelse(regularize, 3, 1)))
+    ret = selectdim(pred, N - 1, 1:(L - ifelse(regularize, 3, 1)))
+
+    return ret
 end
 
 """
@@ -217,14 +261,20 @@ end
 Base.length(d::FFJORDDistribution) = prod(d.model.input_dims)
 Base.eltype(d::FFJORDDistribution) = Lux.recursive_eltype(d.ps)
 
-function Distributions._logpdf(d::FFJORDDistribution, x::AbstractVector)
-    return first(first(__forward_ffjord(d.model, reshape(x, :, 1), d.ps, d.st)))
+function Distributions._logpdf(d::FFJORDDistribution, x::AbstractVector, rng::AbstractRNG=default_rng())
+    return first(first(__forward_ffjord(d.model, reshape(x, :, 1), d.ps, d.st, rng)))
 end
-function Distributions._logpdf(d::FFJORDDistribution, x::AbstractArray)
-    return first(first(__forward_ffjord(d.model, x, d.ps, d.st)))
+function Distributions._logpdf(d::FFJORDDistribution, x::AbstractArray, rng::AbstractRNG=default_rng())
+    return first(first(__forward_ffjord(d.model, x, d.ps, d.st, rng)))
 end
 function Distributions._rand!(
         rng::AbstractRNG, d::FFJORDDistribution, x::AbstractArray{<:Real})
     copyto!(x, __backward_ffjord(eltype(d), d.model, size(x, ndims(x)), d.ps, d.st, rng))
+    return x
+end
+
+function Distributions._rand!(
+        rng::AbstractRNG, d::FFJORDDistribution, x::AbstractVector{<:Real})
+    copyto!(x, reshape(__backward_ffjord(eltype(d), d.model, 1, d.ps, d.st, rng), :))
     return x
 end
